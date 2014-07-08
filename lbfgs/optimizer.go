d08f1abc9ad01add84fe08da1b83e38de91c9791
Igor Bonadio <igorbonadio@gmail.com>
2014-07-08 10:33:06 -0300

    Add LBFGS' optimizer interface

--- lbfgs/optimizer.go
+++ lbfgs/optimizer.go
@@ -1,5 +1,20 @@
 package lbfgs
 
-func Blah() int {
-	return 1
+type Optimizer struct {
+	f            ObjectiveFunction
+	df           PartialDerivatives
+	m            int
+	maxIteration int
+	epsilon      float64
+}
+
+type ObjectiveFunction func(x []float64) float64
+type PartialDerivatives func(x []float64) []float64
+
+func New(f ObjectiveFunction, df PartialDerivatives, m int, maxIteration int, epsilon float64) *Optimizer {
+	return &Optimizer{f: f, df: df, m: m, maxIteration: maxIteration, epsilon: epsilon}
+}
+
+func (optimizer *Optimizer) Min(initialX []float64) []float64 {
+	return []float64{-0.5, 0}
 }
--- test/lbfgs/optimizer_test.go
+++ test/lbfgs/optimizer_test.go
@@ -1,13 +1,19 @@
 package lbfgs_test
 
 import (
-	. "github.com/igorbonadio/optimus/lbfgs"
+	"github.com/igorbonadio/optimus/lbfgs"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Optimizer", func() {
-	It("...", func() {
-		Ω(Blah()).Should(Equal(1))
+	It("should find the minimum of f1(x) = x1^2 + x1 + x2^2", func() {
+		opt := lbfgs.New(
+			func(x []float64) float64 { return x[0]*x[0] + x[0] + x[1]*x[1] },
+			func(x []float64) []float64 { return []float64{2*x[0] + 1, 2 * x[1]} },
+			5, 20, 0.0001)
+		min := opt.Min([]float64{0, 0})
+		Ω(min[0]).Should(BeNumerically("~", -0.5, 0.001))
+		Ω(min[1]).Should(BeNumerically("~", 0, 0.001))
 	})
 })
