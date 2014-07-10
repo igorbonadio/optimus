793ed2d06a89a723e8eac66239323e2fd2f2854d
Igor Bonadio <igorbonadio@gmail.com>
2014-07-10 17:00:17 -0300

    Add lbfg using huichen/lbfgs library

--- lbfgs/optimizer.go
+++ lbfgs/optimizer.go
@@ -1,5 +1,7 @@
 package lbfgs
 
+import "github.com/huichen/lbfgs"
+
 type Optimizer struct {
 	f            ObjectiveFunction
 	df           PartialDerivatives
@@ -16,5 +18,41 @@ func New(f ObjectiveFunction, df PartialDerivatives, m int, maxIteration int, ep
 }
 
 func (optimizer *Optimizer) Min(initialX []float64) []float64 {
-	return []float64{-0.5, 0}
+	opt := lbfgs.NewOptimizer(optimizer.m, len(initialX))
+
+	optX := SF64ToSF32(initialX)
+
+	x := lbfgs.NewVector(len(initialX))
+	g := lbfgs.NewVector(len(initialX))
+	x.SetValues(optX)
+
+	for k := 0; k < optimizer.maxIteration; k++ {
+		g.SetValues(SF64ToSF32(optimizer.df(SF32ToSF64(optX))))
+		delta := opt.GetDeltaX(x, g)
+		x.Increment(delta, 1)
+		for j := 0; j < len(optX); j++ {
+			optX[j] = x.Get(j)
+		}
+		if g.Norm() < float32(optimizer.epsilon) {
+			break
+		}
+	}
+
+	return SF32ToSF64(optX)
+}
+
+func SF64ToSF32(x []float64) []float32 {
+	_x := make([]float32, len(x))
+	for i := 0; i < len(x); i++ {
+		_x[i] = float32(x[i])
+	}
+	return _x
+}
+
+func SF32ToSF64(x []float32) []float64 {
+	_x := make([]float64, len(x))
+	for i := 0; i < len(x); i++ {
+		_x[i] = float64(x[i])
+	}
+	return _x
 }
